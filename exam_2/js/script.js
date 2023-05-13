525c6d9c4d09c30f05a6998222911377bb9fcfcd
Your Name <you@example.com>
2023-05-13 19:34:08 +0300

    Added Google Map

--- exam_2/js/script.js
+++ exam_2/js/script.js
@@ -120,3 +120,26 @@ window.onload = function () {
     offset: 120
   });
 }
+
+// Google Map
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("googleMap"), {
+    center: { lat: 40.6686, lng: -73.8999 },
+    zoom: 13.5,
+    mapId: '2f084b4895741534',
+  });
+
+  const marker = new google.maps.Marker({
+    position: { lat: 40.6781, lng: -73.8981 },
+    map: map,
+    title: "Monticello",
+    icon: {
+      url: "https://i.postimg.cc/30jd33nS/Pin.png",
+      scaledSize: new google.maps.Size(100, 100),
+    },
+  });
+}
+
+window.initMap = initMap;
