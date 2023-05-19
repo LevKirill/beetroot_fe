9fdf993ad4708659896348a225b6d2af172a6d64
Your Name <you@example.com>
2023-05-19 23:44:00 +0300

    Added hw_js_09, added header and main-screen

---
+++ hw_js_09/js/script.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper('.main_screen__swiper', {
+  direction: "vertical",
+  spaceBetween: 30,
+  effect: "cube",
+  grabCursor: true,
+  loop: true,
+  cubeEffect: {
+    shadow: true,
+    slideShadows: true,
+    shadowOffset: 20,
+    shadowScale: 0.94,
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
