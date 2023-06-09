bc7cba537175d49e83b90c0faeac082b5fbd7be4
Lev Kyryl <lev.kyryl@gmail.com>
2023-06-10 00:00:17 +0300

    Finished header

--- exam_3/src/components/layout/Header.js
+++ exam_3/src/components/layout/Header.js
@@ -26,22 +26,32 @@ const links = [
 ]
 
 function Header () {
-  const [isActive, setActive] = useState (false);
-  function toggleBtn () {
-    setActive(!isActive);
+  const [isActiveSearch, setActiveSearch] = useState (false);
+  const [isActiveBurger, setActiveBurger] = useState (false);
+  function toggleSearch () {
+    setActiveSearch(!isActiveSearch);
+    if (isActiveBurger) {
+      setActiveBurger(!isActiveBurger);
+    }
+  }
+
+  function toggleBurger () {
+    setActiveBurger(!isActiveBurger);
+    if (isActiveSearch) {
+      setActiveSearch(!isActiveSearch);
+    }
   }
 
   return (
-    <header className={isActive ? "active" : ""}>
+    <header className={isActiveSearch ? "active" : ""}>
       <div className="wrapper">
         <Link to="/" className="logo"><img src={Logo} alt="Logo"/></Link>
-        <MainMenu links={links} />
         <div
-          id="searchBtn"
-          className={isActive ? "icon_search icon_search__active" : "icon_search"}
-          onClick={toggleBtn}
-        ></div>
-        <div className="hamburger"><span></span></div>
+          className={isActiveBurger ? "hamburger hamburger__active" : "hamburger"}
+          onClick={toggleBurger}
+        ><span></span><span></span><span></span></div>
+        <MainMenu links={links} />
+        <div id="searchBtn" className="icon_search" onClick={toggleSearch}></div>
       </div>
     </header>
   );
--- exam_3/src/components/layout/Search.js
+++ exam_3/src/components/layout/Search.js
@@ -5,7 +5,7 @@ function Search () {
   return (
     <search className="search">
       <div className="wrapper">
-        <form action="" method="GET">
+        <form action="" className="search_form" method="GET">
           <input type="search" name="search" id="search"/>
           <input type="submit" placeholder="I`m looking for..." value="Search"/>
         </form>
